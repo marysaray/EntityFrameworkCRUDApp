4d5e5792d14429cdca758f2919824b5e8881b863
Mary Saray <msaray.ms@outlook.com>
2021-08-06 22:48:11 -0700

    Created Initial code-migration class

---
+++ EntityFrameworkCRUDApp/Migrations/202108070538203_Initial.cs
@@ -0,0 +1,27 @@
+﻿namespace EntityFrameworkCRUDApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Price = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
